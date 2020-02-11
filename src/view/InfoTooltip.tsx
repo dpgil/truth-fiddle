24992c6993d698e9393c0b34ec8515da2fd75e3f
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-02-10 22:28:24 -0500

    Add CustomTooltip to align tooltip styles

--- src/view/InfoTooltip.tsx
+++ src/view/InfoTooltip.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import InfoIcon from '@material-ui/icons/Info';
+import Typography from '@material-ui/core/Typography';
+import { CustomTooltip } from './CustomTooltip';
 
 const iconStyle: React.CSSProperties = {
   display: 'inline-block',
   verticalAlign: 'middle',
+  paddingLeft: 12,
 };
 
 export const InfoTooltip = React.memo(function InfoTooltip() {
-  return <InfoIcon color={'inherit'} style={iconStyle} />;
+  return (
+    <CustomTooltip
+      title={
+        <React.Fragment>
+          <Typography color="inherit">Tooltip with HTML</Typography>
+          <em>{"And here's"}</em> <b>{'some'}</b> <u>{'amazing content'}</u>.{' '}
+          {"It's very engaging. Right?"}
+        </React.Fragment>
+      }
+    >
+      <InfoIcon color={'inherit'} style={iconStyle} />
+    </CustomTooltip>
+  );
 });
