dafb2f8c811622dc51c17e6a79e42812c0faa43b
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-02-10 22:40:48 -0500

    Remove syntaxtable from main page

--- src/view/Title.tsx
+++ src/view/Title.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { InfoTooltip } from './InfoTooltip';
 
+const titleStyle: React.CSSProperties = {
+  marginBottom: 0,
+};
+
 const descriptionStyle: React.CSSProperties = {
   display: 'inline-block',
   verticalAlign: 'middle',
@@ -9,7 +13,7 @@ const descriptionStyle: React.CSSProperties = {
 export const Title = React.memo(function Title() {
   return (
     <>
-      <h1>Truth Fiddle</h1>
+      <h1 style={titleStyle}>Truth Fiddle</h1>
       <div className="rowC">
         <h4 style={descriptionStyle}>A truth table generator.</h4>
         <InfoTooltip />
