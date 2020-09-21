ae9f352d4aa475005c4b1a20197466145c7d52f4
Joe Badlato <joebadlato@gmail.com>
2020-09-20 20:12:15 -0400

    Custom language mode

--- src/view/InputBox.tsx
+++ src/view/InputBox.tsx
@@ -1,8 +1,80 @@
 import React from 'react';
-import CodeMirror from 'react-codemirror';
+import ReactCodeMirror from 'react-codemirror';
+import CodeMirror from 'codemirror';
 require('codemirror/lib/codemirror.css');
 require('./InputBox.css');
 require('codemirror/mode/python/python');
+
+CodeMirror.defineMode('boolean-expressions', function(config, modeOptions) {
+  const KEYWORDS = new Set([
+    'and',
+    'not',
+    'or',
+    'xor',
+    'if',
+    'then',
+    'iff',
+    'only',
+    'true',
+    'false',
+    'equals',
+    'imply',
+    'implies',
+    'nimply',
+    'nimplies',
+    'xnor',
+    'nand',
+    'nor',
+    '<-',
+    '<->',
+    '->',
+    '<=',
+    '=>',
+    '<=>',
+    '<--',
+    '-->',
+    '<-->',
+    '<==>',
+    '<==',
+    '==>',
+    '=',
+    '==',
+    '<=/=',
+    '=/=>',
+    '<-/-',
+    '-/->',
+    '&',
+    '&&',
+    '|',
+    '||',
+    '=/=',
+    '!=',
+    '~=',
+    '^',
+    '\\/',
+    '/\\',
+    '!',
+    '~',
+  ]);
+  return {
+    token: function(stream, state) {
+      const ch = stream.next();
+      if (ch === ' ') {
+        return null;
+      }
+      while (stream.peek() && stream.peek() !== ' ') {
+        stream.next();
+      }
+      const word: string = stream.current();
+      if (KEYWORDS.has(word)) {
+        stream.eat('word: ' + word);
+        return 'keyword';
+      }
+      stream.eat(word);
+      return null;
+    },
+  };
+});
 
 interface InputBoxProps {
   query: string;
@@ -15,13 +87,13 @@ interface InputBoxProps {
 export const InputBox = React.memo(function InputBox(props: InputBoxProps) {
   const codeMirrorOptions = {
     value: 'p and q',
-    mode: { name: 'python' },
+    mode: { name: 'boolean-expressions' },
     viewportMargin: Infinity,
   };
 
   return (
     <div>
-      <CodeMirror
+      <ReactCodeMirror
         value={props.query}
         onChange={props.onQueryChange}
         options={codeMirrorOptions}
